de77377fba7e8f8148db32799bcb706cb8cfd81b
Andrey Adriano <dey2010dey@gmail.com>
2023-12-23 17:39:04 -0300

    cadastrar item

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,14 @@ import './App.css'
 import { FoodData } from './interface/FoodData';
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen]= useState(false);
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className="container">
@@ -19,6 +24,8 @@ function App() {
           />
         )}
       </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>Cadastrar</button>
     </div>
   )
 }
